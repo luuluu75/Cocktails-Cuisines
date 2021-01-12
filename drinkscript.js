b975fba691bdcd73f2cedf563be13c3b25abbb9d
Aks Sreenivasan <aks.auburn@gmail.com>
2021-01-12 21:19:08 +1100

    changes made to drink script file

--- drinkscript.js
+++ drinkscript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var searchedDrinks = JSON.parse(localStorage.getItem("drinks")) || [];
+    var LookupDrinks = JSON.parse(localStorage.getItem("drinks")) || [];
     $("#drinkSearchBtn").on("click", grabInput)
 
     function grabInput() {
@@ -22,17 +22,85 @@ $(document).ready(function() {
             success: function(response) {
                 console.log(response);
                 console.log(queryURL);
-
             }
 
         })
-
     }
 
     function saveDrinksToLS(drinkName) {
-        if (searchedMeals.indexOf(drinkName) === -1) {
-            searchedMeals.push(drinkName);
-            localStorage.setItem("drinks", JSON.stringify(searchedDrinks))
+        if (LookupDrinks.indexOf(drinkName) === -1) {
+            LookupDrinks.push(drinkName);
+            localStorage.setItem("drinks", JSON.stringify(LookupDrinks))
         }
     }
-})
+
+    function createListDrinks(drinkSearch) {
+        $("drink-list").empty();
+
+
+        var keys = Object.keys(drinkSearch);
+        for (var i = 0; i < keys.length; i++) {
+            var drinkName = $("<button>");
+            drinkName.addClass("list-group-item list-group-item-action");
+
+            var splitStr = keys[i].toLowerCase().split(" ");
+            for (var m = 0; m < splitStr.length; m++) {
+                splitStr[m] = splitStr[m].charAt(0).toUpperCase() + splitStr[m].substring(1);
+            }
+
+            var titleDrink = splitStr.join(" ");
+            drinkName.text(titleDrink);
+            $("#drink-list").append(drinkName);
+        }
+
+    }
+
+    createListDrinks();
+    // var drinkName = $("<h1>").text(drinkName.);
+    // $("#drinkName").prepend(drinkName);
+
+    // var drinkIcon = $("<img>");
+    // drinkIcon.attr(
+    //     "src",
+    //     "https://www.thecocktaildb.com/images/media/drink/?s=${drinks}.jpg/preview(100x100 pixels)" + drinkName.drinks[0].icon + ".jpg"
+    // );
+
+    // console.log(drinkIcon.list[i].drinkName[0].icon);
+
+    // $("#drinkName").append(drinkIcon);
+    // function createListDrinks(drinkSearch) {
+    //     $("drinkName").empty();
+
+    //     var keys = Object.keys(drinkSearch);
+    //     for (var i = 0; i < keys.length; i++) {
+    //         var drinkEntry = $("<button>");
+    //         drinkEntry.addClass("list-group-item list-group-item-action");
+
+    //         var splitStr = keys[i].toLowerCase().split(" ");
+    //         for (var m = 0; m < splitStr.length; m++) {
+    //             splitStr[m] = splitStr[m].charAt(0).toUpperCase() + splitStr[m].substring(1);
+    //         }
+
+    //         var titleDrink = splitStr.join(" ");
+    //         DrinkEntry.text(titleDrink);
+    //         $("#drink-list").append(drinkEntry);
+    //     }
+
+    // }
+    // createListDrinks();
+
+
+    // $("#drink-name").empty();
+
+
+    // var drinkName = $("<h3>").text(drinks.name); //h3
+    // $("#drinkname").prepend(drinkName);
+
+    // var drinkIcon = $("<img>");
+    // drinkIcon.attr(
+    //     "src",
+    //     "https://www.thecocktaildb.com/images/media/drink/preview(100x100 pixels)" + drinks.drink[0].icon + ".jpg"
+    // );
+
+
+});
