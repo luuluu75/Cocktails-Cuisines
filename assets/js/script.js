cc9c9c6348aa02de1423e3d43a80445277f541fd
Mona Lisa <clarkefraser1@gmail.com>
2021-01-16 14:17:16 +1100

    Updating class name to append to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
         console.log(searchResults);
         var arrayItem = searchResults.find(element => element.idMeal === datTest);
         console.log(arrayItem);
+        $(".accordion-content").append(arrayItem);
 		
 		
 		
@@ -63,7 +64,7 @@ $(document).ready(function() {
                 var callMeals = `<li class="mealClass" data-mealid="${response.meals[i].idMeal}"'>${response.meals[i].strMeal} </li> `;
                 // (response.meals[i].strMeal);
                 //console.log(callMeals);
-                $('#searchResults').append(callMeals);
+                $('#searchResultsTab').append(callMeals);
 
                 
                 
