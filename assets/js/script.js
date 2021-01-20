9d6e24ad314e8cd952f25a595b0bfc15c9a64a4d
Mona Lisa <clarkefraser1@gmail.com>
2021-01-20 19:37:48 +1100

    fixed accordion & search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-            var searchResults;
-            var responseMeals;
-            var responseDrinks;
+    var searchResults;
+    var responseMeals;
+    var responseDrinks;
 
-            var searchedMeals = JSON.parse(localStorage.getItem("meals")) || [];
-            var LookupDrinks = JSON.parse(localStorage.getItem("drinks")) || [];
+    var searchedMeals = JSON.parse(localStorage.getItem("meals")) || [];
+    var LookupDrinks = JSON.parse(localStorage.getItem("drinks")) || [];
 
-            //console.log($(".dropdownMenu").val()) //default
+    //console.log($(".dropdownMenu").val()) //default
 
-            $("#searchBtn").on("click", function() {
-                if ($(".dropdownMenu").val() === "meals") {
-                    var meals = $("#searchBar").val();
-                    searchMeals(meals);
-                } else if ($(".dropdownMenu").val() === "drinks") {
-                    var drinks = $("#searchBar").val();
-                    searchDrinks(drinks);
+    $("#searchBtn").on("click", function () {
+        if ($(".dropdownMenu").val() === "meals") {
+            var meals = $("#searchBar").val();
+            searchMeals(meals);
+        } else if ($(".dropdownMenu").val() === "drinks") {
+            var drinks = $("#searchBar").val();
+            searchDrinks(drinks);
+        }
+    })
+
+    function grabInput(event) {
+
+        event.preventDefault();
+        if ($(this).attr("type") === "meal") {
+            var mealId = $(this).attr("meal-id");
+            console.log(mealId);
+            console.log(responseMeals);
+
+            
+            var result = responseMeals.find(meal => meal.idMeal === mealId);
+            console.log(result);
+            var recipeInstruction = $(`<div>${result.strInstructions} </div>`)
+            console.log(recipeInstruction);
+            var recipeImage = `<img class="image" src= ${result.strMealThumb}>`;
+            console.log(recipeImage);
+
+            
+
+            //var recipeInstruction = `<div> <li> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} </li> </div>`;
+
+            // Get all ingredients from the object. Up to 20
+            const ingredients = [];
+            for (let i = 1; i <= 20; i++) {
+                if (result[`strIngredient${i}`]) {
+                    ingredients.push(`${result[`strIngredient${i}`]} - ${result[`strMeasure${i}`]}`);
+                } else {
+                    // Stop if there are no more ingredients
+                    break;
                 }
-            })
+            }
 
-            function grabInput(event) {
-
-                event.preventDefault();
-                if ($(".dropdownMenu").val() === "meals") {
-                    var mealId = $(this).attr("meal-id");
-                    console.log(mealId);
-                    console.log(responseMeals);
-
-                    // var clickedMeal = responseMeals.filter(function(meal){
-                    //     return meal.idMeal === mealId;
-                    //console.log(clickedMeal);
-                    var result = responseMeals.find(meal => meal.idMeal === mealId);
-                    console.log(result);
-                    var recipeInstruction = $(`<div>${result.strInstructions} </div>`)
-                    console.log(recipeInstruction);
-                    var recipeImage = `<img class="image" src= ${result.strMealThumb}>`;
-                    console.log(recipeImage);
-
-                    // const instructions = [];
-
-                    // for (let line = 1; line <= 1000; line++) 
-                    // {
-                    //     if (result[`strInstructions${line}`]) {
-                    // //         instructions.push(`${result[`strInstructions${i}`]}`);
-                    // // } else {
-                    // //     // Stop if there are no more instructions
-                    // //     break;
-                    // // }
-                    // // }
-
-
-                    //var recipeInstruction = `<div> <li> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} </li> </div>`;
-
-                    // Get all ingredients from the object. Up to 20
-                    const ingredients = [];
-                    for (let i = 1; i <= 20; i++) {
-                        if (result[`strIngredient${i}`]) {
-                            ingredients.push(`${result[`strIngredient${i}`]} - ${result[`strMeasure${i}`]}`);
-                    } else {
-                        // Stop if there are no more ingredients
-                        break;
-                    }
-                    }
-                    
             // var recipeInstruction = `<li> ${instructions.map(instruction => `<li>${instruction}</li>`).split('.')} </li>`;
-           var recipeIngredient = `<li> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} </li>`;
-           console.log(recipeIngredient);
+            var recipeIngredient = `<li> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} </li>`;
+            console.log(recipeIngredient);
 
 
             //strYoutube     strMealThumb
@@ -74,10 +61,7 @@ $(document).ready(function() {
             console.log($('.accordion-content'));
 
 
-            // if ($(this).children().length < 3) {
-            //    $(this).append(recipeInstruction);
-            //  $(this).append(recipeImage);
-            //  };
+            
             if ($(this).find(".panel").children().length < 1) {
                 $(this).find(".panel").append(recipeIngredient);
                 $(this).find(".panel").append(recipeInstruction);
@@ -91,31 +75,19 @@ $(document).ready(function() {
 
             //strInstructions
             // })
-            for (var i = 0; i < 0; i++) 
-            {
-                console.log(clickedMeal[i]);
 
-            }
             var acc = document.getElementsByClassName("accordion-item");
             var i;
 
-            for (i = 0; i < acc.length; i++) {
-                    acc[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
+           
 
 
-                });
-            }
-
-
-        } else if ($(".dropdownMenu").val() === "drinks") {
+        } else if ($(this).attr("type") === "drinks")  {
             var drinkId = $(this).attr("drink-id");
             console.log(drinkId);
             console.log(responseDrinks);
 
-            // var clickedMeal = responseMeals.filter(function(meal){
-            //     return meal.idMeal === mealId;
-            //console.log(clickedMeal);
+            
             var result = responseDrinks.find(drink => drink.idDrink === drinkId);
             console.log(result);
 
@@ -124,35 +96,27 @@ $(document).ready(function() {
             console.log(recipeInstruction);
             var recipeImage = `<img class="image" src= ${result.strDrinkThumb}>`;
             console.log(recipeImage);
-            //for loop with i
-            //for J i=number
-            //strIngredients + 
-            //if(!$(this).text()){$(this).append(result)};
-            //console.log(this.text);
+            
             const ingredients = [];
             for (let i = 1; i <= 20; i++) {
                 if (result[`strIngredient${i}`]) {
                     ingredients.push(`${result[`strIngredient${i}`]} - ${result[`strMeasure${i}`]}`);
-            } else {
-                // Stop if there are no more ingredients
-                break;
+                } else {
+                    // Stop if there are no more ingredients
+                    break;
+                }
             }
-            }
-            
+
 
             var recipeIngredient = `<li> ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} </li>`;
-           console.log(recipeIngredient);
-            
+            console.log(recipeIngredient);
+
 
             //strYoutube     strMealThumb
             //$(this).append(recipeInstruction);
             console.log($(this).children());
             console.log($('.accordion-content'));
-            //$(!$(this).text()(recipeImage);
-            // if ($(this).children().length < 3) {
-            //     $(this).append(recipeInstruction);
-            //     $(this).append(recipeImage);
-            // };
+           
             if ($(this).find(".panel").children().length < 1) {
                 $(this).find(".panel").append(recipeIngredient);
                 $(this).find(".panel").append(recipeInstruction);
@@ -162,9 +126,7 @@ $(document).ready(function() {
             $('#searchResultsTab').foundation('toggle', $(this).find(".panel"));
 
 
-            // $('#searchResultsTab').foundation('toggle', $(`.accordion-content`));
-            //strInstructions
-            // })
+            
             for (var i = 0; i < 0; i++) {
                 console.log(clickedDrink[i]);
 
@@ -173,8 +135,7 @@ $(document).ready(function() {
             var i;
 
             for (i = 0; i < acc.length; i++) {
-                acc[i].addEventListener("click", function() 
-                {
+                acc[i].addEventListener("click", function () {
                     this.classList.toggle("active");
 
 
@@ -187,59 +148,16 @@ $(document).ready(function() {
     function registerClickListeners() {
         $(".accordion-item").on('click', grabInput);
 
-    } //searchedMeals($(this).text());
+    } 
 
-    // if (($(".dropdownMenu").val() === "meals")) 
-    // {
-    //     $("#searchBar").on("keypress", function() {
-    //         if (event.keyCode === 13) {
-    //             var meal = ($(this)).val();
-    //             console.log(meal);
-    //             searchMeals(meal);
-    //         }
-    //     })
-    // } else if (($(".dropdownMenu").val() === "drinks")) 
-    // {
-    //     $("#searchBar").on("keypress", function() {
-    //         if (event.keyCode === 13) {
-    //             var drink = ($(this)).val();
-    //             console.log(drink);
-    //             searchDrinks(drink);
-    //         }
-    //     })
-    // }
-
-    // if (($(".dropdownMenu").val() === "meals")) 
-    // {
-    //     $("#searchBar").on("keypress", function() {
-    //         if (event.keyCode === 13) {
-    //             var meal = ($(this)).val();
-    //             console.log(meal);
-    //             searchMeals(meal);
-    //         }
-    //     })
-    // } else if (($(".dropdownMenu").val() === "drinks")) 
-    // {
-    //     $("#searchBar").on("keypress", function() {
-    //         if (event.keyCode === 13) {
-    //             var drink = ($(this)).val();
-    //             console.log(drink);
-    //             searchDrinks(drink);
-    //         }
-    //     })
-    // }
-
-    $("#searchBtn").on("keypress", function() 
-    {
-        if ($(".dropdownMenu").val() === "meals") 
-        {
+    $("#searchBtn").on("keypress", function () {
+        if ($(".dropdownMenu").val() === "meals") {
             if (event.keyCode === 13) {
                 var meal = ($(this)).val();
                 console.log(meal);
                 searchMeals(meal);
             }
-        } else if ($(".dropdownMenu").val() === "drinks") 
-        {
+        } else if ($(".dropdownMenu").val() === "drinks") {
             if (event.keyCode === 13) {
                 var drink = ($(this)).val();
                 console.log(drink);
@@ -247,9 +165,7 @@ $(document).ready(function() {
             }
         }
     });
-    // $("#drinksName").on("keypress", function(){
-
-    // })
+    
 
 
     function searchMeals(meals) {
@@ -261,23 +177,19 @@ $(document).ready(function() {
             type: "GET",
             url: queryURL,
             dataType: "json",
-            success: function(response) {
+            success: function (response) {
                 responseMeals = response.meals
                 console.log(response.meals);
                 //console.log(queryURL);
                 //searchResults=response.meals;
                 for (var i = 0; i < 5; i++) {
-                    //  console.log(response.meals[i]);
-                    //   let meal = `<li class=\"accordion-item\" meal-id = ${response.meals[i].idMeal} data-accordion-item data-allow-all-closed='true' >
-                    //       <!-- Accordion tab title -->
-                    //       <a href=\"#\" class=\"accordion-title\">${response.meals[i].strMeal} </a>
-                    //     </li>`
-                    if (response.meals[i] != undefined)
-                    { var accordianMealTest =
-                        `<div class="grid-container" id="mainContent">
+                   
+                    if (response.meals[i] != undefined) {
+                        var accordionMealTest =
+                            `<div class="grid-container" id="mainContent">
                 
                     <ul class="accordion" data-accordion data-allow-all-closed="true" >
-                        <li class="accordion-item" data-accordion-item meal-id= ${response.meals[i].idMeal} >
+                        <li class="accordion-item" data-accordion-item type="meal" meal-id= ${response.meals[i].idMeal} >
                         <!-- Accordion tab title -->
                         <a href="#" class="accordion-title ">${response.meals[i].strMeal}</a>
                         <div class="accordion-content panel" data-tab-content>
@@ -287,27 +199,18 @@ $(document).ready(function() {
                         </li>
                     </ul>
             </div>`
-                        //console.log(accordianMealTest);
+                        
+                        $('#searchResultsTab').append(accordionMealTest);
 
-                    //console.log(meal);
-
-                    // var callMeals = `<li class="mealClass" data-mealid="${response.meals[i].idMeal}"'>${response.meals[i].strMeal} </li> `;
-                    // // (response.meals[i].strMeal);
-                    // //console.log(callMeals);
-                    // $('#searchResultsTab').append(callMeals);
-                    $('#searchResultsTab').append(accordianMealTest);
+                    }
 
                 }
-                        
-                   
-
-                //click listener
+                
                 registerClickListeners()
             }
 
-        }
-
-    })}
+        })
+    }
 
     function searchDrinks(drinks) {
         saveDrinksToLS(drinks);
@@ -318,7 +221,7 @@ $(document).ready(function() {
             type: "GET",
             url: queryURL2,
             dataType: "json",
-            success: function(response) {
+            success: function (response) {
                 responseDrinks = response.drinks
                 console.log(response.drinks);
                 //console.log(queryURL);
@@ -327,11 +230,11 @@ $(document).ready(function() {
                 for (var i = 0; i < 5; i++) {
 
                     if (response.drinks[i] != undefined) {
-                        var accordianDrinkTest =
+                        var accordionDrinkTest =
                             `<div class="grid-container" id="mainContent">
                 
                     <ul class="accordion" data-accordion data-allow-all-closed="true" >
-                        <li class="accordion-item" data-accordion-item drink-id= ${response.drinks[i].idDrink} >
+                        <li class="accordion-item" data-accordion-item type="drinks" drink-id= ${response.drinks[i].idDrink} >
                         <!-- Accordion tab title -->
                         <a href="#" class="accordion-title ">${response.drinks[i].strDrink}</a>
                         <div class="accordion-content panel" data-tab-content>
@@ -342,32 +245,11 @@ $(document).ready(function() {
                     </ul>
             </div>`
 
-                        $('#searchResultsTab').append(accordianDrinkTest);
+                        $('#searchResultsTab').append(accordionDrinkTest);
 
                     }
 
-                    //while (responseDrinks)
-
-                    // console.log(response.drinks);
-                    // console.log([i]);
-                    //         var accordianDrinkTest =
-                    //             `<div class="grid-container" id="mainContent">
-
-                    //         <ul class="accordion" data-accordion data-allow-all-closed="true" >
-                    //             <li class="accordion-item" data-accordion-item drink-id= ${response.drinks[i].idDrink} >
-                    //             <!-- Accordion tab title -->
-                    //             <a href="#" class="accordion-title ">${response.drinks[i].strDrink}</a>
-                    //             <div class="accordion-content panel" data-tab-content>
-
-
-                    //             </div>
-                    //             </li>
-                    //         </ul>
-                    // </div>`
-
-                    //         $('#searchResultsTab').append(accordianDrinkTest);
-
-
+                    
 
 
 
